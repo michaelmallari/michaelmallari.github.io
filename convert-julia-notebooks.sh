6e26636dbb04cc1274028162ee8af8541a02baae
Michael Mallari <hello@michaelmallari.com>
2020-06-28 11:40:08 -0600

    Added Jupyter con notebook converter

--- convert-julia-notebooks.sh
+++
@@ -1,6 +0,0 @@
-FILES="$(find julia-notebooks -type f -name '*.ipynb')"
-for f in $FILES
-do
-    nb2hugo $f --site-dir base --section julia
-done
-hugo -s base
--- convert-python-notebooks.sh
+++
@@ -1,6 +0,0 @@
-FILES="$(find python-notebooks -type f -name '*.ipynb')"
-for f in $FILES
-do
-    nb2hugo $f --site-dir base --section python
-done
-hugo -s base
